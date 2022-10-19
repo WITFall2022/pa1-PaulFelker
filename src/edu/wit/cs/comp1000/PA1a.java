65ee2593cd70fb5f8c2cc09721b4636f2e48fd98
pwfel <pwfel@192.168.1.104>
2022-10-18 21:52:20 -0400

    Added my source code to PA1a

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,33 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;//Scanner is in the java.util package
+
+
 public class PA1a {
 
 	// TODO: document this function
 	public static void main(String[] args) {
 		// TODO: write your code here
+Scanner input = new Scanner(System.in);
+		
+		//Prompt the user to enter yards, feet and inches.
+		
+		System.out.println("Enter the number of yards:");
+		int yards = input.nextInt();
+		
+		System.out.println("Enter the number of feet:");
+		int feet = input.nextInt();
+		
+		System.out.println("Enter the number of inches:");
+		int inches = input.nextInt();
+		
+		//Compute total number of inches.
+		
+		int Sum = (yards*36 + feet*12 + inches);
+		
+		//Display results.
+		System.out.print("The total number of inches is:" + Sum);
+
 	}
 
 }
