d4c1b87528094bb3e0195796f8ec7924fdf43e37
pwfel <pwfel@192.168.1.104>
2022-10-20 08:54:23 -0400

    Changed comments.

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -4,7 +4,7 @@ import java.util.Scanner;//Scanner is in the java.util package
 
 
 public class PA1a {
-	// Class identifier.
+	// Class identifier and name.
 
 	
 	public static void main(String[] args) {
